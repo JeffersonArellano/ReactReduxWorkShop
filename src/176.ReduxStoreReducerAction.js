27e9dd7d750f4b5f08681a3a7596885d6ea38e32
Jefferson Arellano <Jefferson.Arellano@travelport.com>
2020-07-16 15:40:47 +0200

    first commit

---
+++ src/176.ReduxStoreReducerAction.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Component = () => {
+
+    return (
+        <div>
+            Default component
+        </div>
+    )
+}
+
+export default Component;
+
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Component from './176.ReduxStoreReducerAction'
+
+function App() {
+  return (
+    <Component />
+  );
+}
+
+export default App;
